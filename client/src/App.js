a39ca6d3c0e5a884b4a8c5b5fa2039a25356478f
Clay0277 <claytonallmond@gmail.com>
2023-03-09 22:35:01 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    fetch("https://name-generator-mk52.onrender.com/generate")
+      .then((res) => res.json())
+      .then((name) => setName([name]));
+  }, []);
+
+  return <h1>{name}</h1>;
+};
+
+export default App;
